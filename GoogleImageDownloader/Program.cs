53daec66b73cc427bd0fa20dfe08f26f64f4db01
maifeeulasad <maifeeulasad@gmail.com>
2019-01-30 12:06:46 +0600

    Update Program.cs

--- GoogleImageDownloader/Program.cs
+++ GoogleImageDownloader/Program.cs
@@ -12,11 +12,15 @@ namespace GoogleImageDownloader
 {
     class Program
     {
-
+        static int cou = 0;
         
         
         static void Main(string[] args)
         {
+            cou = 0;
+
+            string loc = @"C:\Users\MUA\Downloads\dom\";
+
             string x = W.GetData("hello world");
 
             //Debug.WriteLine(x);
@@ -26,10 +30,29 @@ namespace GoogleImageDownloader
             List<string> dd = ParseHTML.rData;
 
 
+            using (WebClient client = new WebClient())
+            {
+
+                client.DownloadFile(new Uri("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkhuJUVjHM9DPgNGwSs3dgsMvq7Oa-m02gav39X3IL6yf_QMR3SB50MzeC"), @"C:\Users\MUA\Downloads\dom\1.png");
+
+
+            }
+
+
+            /*
             foreach(string zz in dd)
             {
-                Debug.WriteLine(zz);
+                using (WebClient client = new WebClient())
+                {
+
+                    //client.DownloadFile(new Uri(zz), loc + cou.ToString() + ".png");
+
+
+                }
+
+                //Debug.WriteLine(zz);
             }
+            */
 
         }
 
