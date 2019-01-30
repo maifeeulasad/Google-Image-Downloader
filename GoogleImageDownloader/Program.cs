e80993ec038aa3ffdbb223abb384c0c4b6eedc10
maifeeulasad <maifeeulasad@gmail.com>
2019-01-30 10:12:02 +0600

    basic improvement

--- GoogleImageDownloader/Program.cs
+++ GoogleImageDownloader/Program.cs
@@ -4,42 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GoogleImageDownloader
 {
     class Program
     {
-        static string GoogleURL(string name)
-        {
-            string[] p = name.Split();
-            string x = "";
-            foreach(string y in p)
-            {
-                x += y + "+";
-            }
-            x = x.Substring(0, x.Length - 1);
-            return "https://www.google.com/search?q="+x+"&source=lnms&tbm=isch&sa=X";
-        }
 
-        static int cou=0;
+        
+        
         static void Main(string[] args)
         {
-            string name = "hello world";
-            string basicLoc = @"F:\c_sharp\google_image_downloader\New folder\";
-            /*
-             * https://www.google.com/search?q=hello+world&source=lnms&tbm=isch&sa=X
-             */
 
-            string remoteFileUrl = GoogleURL(name);
-            string localFileName = basicLoc + (++cou).ToString() + ".jpg";
-       
-
-           
-
-            WebClient webClient = new WebClient();
-            webClient.DownloadFile(remoteFileUrl, localFileName);
-
+            W w = new W();
+            w.Load("www.google.com");
         }
+
+        
     }
 }
---
+++ GoogleImageDownloader/W.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleImageDownloader
+{
+    public class W
+    {
+        WebBrowser web;
+
+        public W()
+        {
+            Init();
+        }
+        public W(string url)
+        {
+            Init();
+            Load(url);
+
+        }
+
+        void Init()
+        {
+            var t = new Thread(I);
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+
+        }
+
+        void I()
+        {
+            web = new WebBrowser();
+            web.DocumentCompleted += Web_DocumentCompleted;
+        }
+
+        private void Web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Show();
+            //throw new NotImplementedException();
+        }
+
+        public void Load(string url)
+        {
+            web.Navigate(url);
+
+            for(int i=0;i<100000;i++)
+            {
+
+            }
+        }
+
+
+
+
+        void Show()
+        {
+            var c = web.Document.Images;
+            foreach (var x in c)
+            {
+                Debug.WriteLine(x);
+            }
+        }
+        
+
+    }
+}
+
