50d8c8bcaab56ef7a3210f81ac105a02020e714b
maifeeulasad <maifeeulasad@gmail.com>
2019-01-30 12:52:55 +0600

    works fine

--- GoogleImageDownloader/Program.cs
+++ GoogleImageDownloader/Program.cs
@@ -21,38 +21,28 @@ namespace GoogleImageDownloader
 
             string loc = @"C:\Users\MUA\Downloads\dom\";
 
-            string x = W.GetData("hello world");
+            string se = Console.ReadLine();
 
-            //Debug.WriteLine(x);
+            string x = W.GetData(se);
+
+
 
             ParseHTML ll = new ParseHTML(x, "img");
 
             List<string> dd = ParseHTML.rData;
 
 
-            using (WebClient client = new WebClient())
-            {
-
-                client.DownloadFile(new Uri("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkhuJUVjHM9DPgNGwSs3dgsMvq7Oa-m02gav39X3IL6yf_QMR3SB50MzeC"), @"C:\Users\MUA\Downloads\dom\1.png");
+            
 
 
-            }
-
-
-            /*
+            
             foreach(string zz in dd)
             {
-                using (WebClient client = new WebClient())
-                {
+                Util.Download(zz, @"C:\Users\MUA\Downloads\dom", (cou++).ToString(),se);
 
-                    //client.DownloadFile(new Uri(zz), loc + cou.ToString() + ".png");
-
-
-                }
-
-                //Debug.WriteLine(zz);
+                Debug.WriteLine(zz);
             }
-            */
+
 
         }
 
---
+++ GoogleImageDownloader/Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleImageDownloader
+{
+    class Util
+    {
+
+        public static void Download(string url,string location,string serial,string se)
+        {
+
+            Directory.CreateDirectory(location + "\\" + se);
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadFile(new Uri(url), location + "\\"+se + "\\" + serial + ".png");
+
+                }
+                catch(Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+
+
+
+            }
+        }
+
+
+    }
+}
