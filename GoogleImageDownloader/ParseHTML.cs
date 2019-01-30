8eb9fb06d61a69fc567f0c32d8fc6edef351a78f
maifeeulasad <maifeeulasad@gmail.com>
2019-01-30 11:56:29 +0600

    Update ParseHTML.cs

--- GoogleImageDownloader/ParseHTML.cs
+++ GoogleImageDownloader/ParseHTML.cs
@@ -24,8 +24,24 @@ namespace GoogleImageDownloader
                 int y = data.Substring(x).IndexOf('>');
                 string tem = data.Substring(x, y + 1);
 
+
+                rData.Add(GetAttributeContent(tem, "src"));
+                //Debug.WriteLine(tem);
+
+                //Debug.WriteLine(GetAttributeContent(tem, "src"));
+
             }
 
+        }
+
+
+        public static string GetAttributeContent(string data,string attribute)
+        {
+            int pos = data.IndexOf(attribute);
+            data = data.Substring(pos + 5);
+            data = data.Substring(0, data.IndexOf('"'));
+
+            return data;
         }
 
         public static List<int> AllIndexesOf(string str, string value)
